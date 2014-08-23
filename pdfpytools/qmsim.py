86008c3b2577cd2c0b57005f59a6cbfa2c831716
Toshiyuki HIRANO <hiracchi@gmail.com>
2014-08-23 12:59:36 +0900

    Fix: for release 2014.0rc

--- pdfpytools/qmsim.py
+++ pdfpytools/qmsim.py
@@ -19,36 +19,18 @@
 # You should have received a copy of the GNU General Public License
 # along with ProteinDF.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-report machine environments
-"""
+class QmSim(object):
+    """
+    QmSimulatorの基本クラス
+    """
+    def __init__(self):
+        self._data = {}
 
-import sys
-import os.path
-import argparse
-import logging
+    # properies ================================================================
+    # method
+    def _get_method(self):
+        return self._data.get('method', 'hf')
+    def _set_method(self, method):
+        self._data['method'] = str(method)
+    method = property(_get_method, _set_method)
 
-def main():
-    parser = argparse.ArgumentParser(description='report environments')
-    parser.add_argument('-v', '--verbose',
-                        action='store_true',
-                        default=False)
-    parser.add_argument('-d', '--debug',
-                        action='store_true',
-                        default=False)
-    args = parser.parse_args()
-    verbose = args.verbose
-    logging.basicConfig()
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.WARNING)
-
-        
-    # 環境変数の表示
-    for k, v in os.environ.items():
-        print("{key} : {value}".format(key=k, value=v))
-        
-
-if __name__ == '__main__':
-    main()
