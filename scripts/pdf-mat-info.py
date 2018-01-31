97f9fc1950b889837a018f0a1e5a482f15f49afa
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2018-01-31 14:49:29 +0900

    feat: add scripts
    - add script: pdf-mat-info
    - support profiler in pdf-report
    - add script: pdf-show-profile

---
+++ scripts/pdf-mat-info.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import pdfpytools as pdf
+
+def main():
+    # parse args
+    parser = argparse.ArgumentParser(description='show matrix info')
+    parser.add_argument("path",
+                        nargs=1,
+                        help='matrix path')
+    parser.add_argument("-v", "--verbose",
+                        action="store_true",
+                        default=False)
+    parser.add_argument('-D', '--debug',
+                        action='store_true',
+                        default=False)
+    args = parser.parse_args()
+
+    # setting
+    mat_path = args.path[0]
+    verbose = args.verbose
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if verbose:
+        print("loading: {}".format(mat_path))
+
+    stdmat = pdf.Matrix()
+    symmat = pdf.SymmetricMatrix()
+    if stdmat.is_loadable(mat_path):
+        stdmat.load(mat_path)
+        print("standard dens matrix")
+        print("size: {row} x {col}".format(row = stdmat.rows,
+                                           col = stdmat.cols))
+        #print(stdmat)
+    elif symmat.is_loadable(mat_path):
+        symmat.load(mat_path)
+        print("symmetric dens matrix")
+        print("size: {row} x {col}".format(row = symmat.rows,
+                                           col = symmat.cols))
+        #print(symmat)
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 
 # Copyright (C) 2014 The ProteinDF development team.
 # see also AUTHORS and README if provided.
-# 
+#
 # This file is a part of the ProteinDF software package.
-# 
+#
 # The ProteinDF is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # The ProteinDF is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with ProteinDF.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -35,16 +35,18 @@ setup(name='ProteinDF_pytools',
           'scripts/pdf',
           'scripts/pdf-archive.py',
           'scripts/pdf-env.py',
-	  'scripts/pdf-info-geom.py',
+          'scripts/pdf-info-geom.py',
           'scripts/pdf-info-orb.py',
-	  'scripts/pdf-info-xyz.py',
+          'scripts/pdf-info-xyz.py',
           'scripts/pdf-make-basis2.py',
+          'scripts/pdf-mat-info.py',
           'scripts/pdf-plot-basisset.py',
           'scripts/pdf-plot-elevel.py',
           'scripts/pdf-plot-mat.py',
           'scripts/pdf-plot-decaymat.py',
           'scripts/pdf-report.py',
-	  'scripts/pdf-reg-harris.py',
+          'scripts/pdf-reg-harris.py',
+          'scripts/pdf-show-profile.py',
           'scripts/pdf-test.py',
           'scripts/pdf-test-eri.py',
           'scripts/g0xeri2mpac.py',
@@ -59,4 +61,3 @@ setup(name='ProteinDF_pytools',
           ('data', ['data/basis2.cache'])
       ]
 )
-
