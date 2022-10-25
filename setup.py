f5cdd124c0fc3aaae1dcc5455d375cf4a08a96d0
Toshiyuki HIRANO <hiracchi@users.noreply.github.com>
2022-10-25 23:07:46 +0900

    feat: current pcakcage format

--- setup.py
+++ setup.py
@@ -19,70 +19,73 @@
 # You should have received a copy of the GNU General Public License
 # along with ProteinDF.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-import os
-import shutil
-from setuptools import setup, find_packages
 
-exec(open("proteindf_tools/_version.py").read())
+from setuptools import setup
 
-shutil.copyfile("scripts/pdfcmd.py", "scripts/pdf")
+setup()
 
-setup(
-    name="proteindf_tools",
-    version=__version__,
-    description="python scripts for ProteinDF package",
-    author="Toshiyuki HIRANO",
-    author_email="hiracchi@gmail.com",
-    url="http://proteindf.github.io/",
-    license="GPLv3",
-    packages=["proteindf_tools"],
-    scripts=[
-        "scripts/pdf",
-        "scripts/pdf-archive.py",
-        "scripts/pdf-archive-h5.py",
-        "scripts/pdf-atom-index.py",
-        "scripts/pdf-env.py",
-        "scripts/pdf-estimate-ao.py",
-        "scripts/pdf-info-geom.py",
-        "scripts/pdf-info-iteration.py",
-        "scripts/pdf-info-orb.py",
-        "scripts/pdf-info-xyz.py",
-        "scripts/pdf-make-basis2.py",
-        "scripts/pdf-mat-info.py",
-        "scripts/pdf-mat-show.py",
-        "scripts/pdf-plot-basisset.py",
-        "scripts/pdf-plot-elevel.py",
-        "scripts/pdf-plot-elevel-tracer.py",
-        "scripts/pdf-plot-mat.py",
-        "scripts/pdf-plot-dos.py",
-        "scripts/pdf-plot-pop.py",
-        "scripts/pdf-plot-decaymat.py",
-        "scripts/pdf-report.py",
-        "scripts/pdf-report-h5.py",
-        "scripts/pdf-reg-harris.py",
-        "scripts/pdf-show-profile.py",
-        "scripts/pdf-test.py",
-        "scripts/pdf-test-h5.py",
-        "scripts/pdf-test-eri.py",
-        "scripts/xyz2gau.py",
-        "scripts/xyz2pdf.py",
-        "scripts/g0xeri2mpac.py",
-        "scripts/pdf-pop-classo.py",
-        "scripts/pdf-pop-ridge.py",
-        "scripts/pdf-pop-resp.py",
-        "scripts/pdf-pop-rrms.py",
-        "scripts/pdf-set-charges.py",
-    ],
-    install_requires=[
-        "h5py",
-        "BeautifulSoup4",
-        "numpy",
-        "scipy",
-        "pandas",
-        "matplotlib",
-        "scikit-learn",
-        # 'proteindf_bridge'
-    ],
-    package_data={"proteindf_tools": ["data/basis2.cache"]},
-)
+# import sys
+# import os
+# import shutil
+# from setuptools import setup, find_packages
+
+# exec(open("proteindf_tools/_version.py").read())
+
+# shutil.copyfile("scripts/pdfcmd.py", "scripts/pdf")
+
+# setup(
+#     name="proteindf_tools",
+#     version=__version__,
+#     description="python scripts for ProteinDF package",
+#     author="Toshiyuki HIRANO",
+#     author_email="hiracchi@gmail.com",
+#     url="http://proteindf.github.io/",
+#     license="GPLv3",
+#     packages=["proteindf_tools"],
+#     scripts=[
+#         "scripts/pdf",
+#         "scripts/pdf-archive.py",
+#         "scripts/pdf-archive-h5.py",
+#         "scripts/pdf-atom-index.py",
+#         "scripts/pdf-env.py",
+#         "scripts/pdf-estimate-ao.py",
+#         "scripts/pdf-info-geom.py",
+#         "scripts/pdf-info-orb.py",
+#         "scripts/pdf-info-xyz.py",
+#         "scripts/pdf-make-basis2.py",
+#         "scripts/pdf-mat-info.py",
+#         "scripts/pdf-mat-show.py",
+#         "scripts/pdf-plot-basisset.py",
+#         "scripts/pdf-plot-elevel.py",
+#         "scripts/pdf-plot-mat.py",
+#         "scripts/pdf-plot-dos.py",
+#         "scripts/pdf-plot-pop.py",
+#         "scripts/pdf-plot-decaymat.py",
+#         "scripts/pdf-report.py",
+#         "scripts/pdf-report-h5.py",
+#         "scripts/pdf-reg-harris.py",
+#         "scripts/pdf-show-profile.py",
+#         "scripts/pdf-test.py",
+#         "scripts/pdf-test-h5.py",
+#         "scripts/pdf-test-eri.py",
+#         "scripts/xyz2gau.py",
+#         "scripts/xyz2pdf.py",
+#         "scripts/g0xeri2mpac.py",
+#         "scripts/pdf-pop-classo.py",
+#         "scripts/pdf-pop-ridge.py",
+#         "scripts/pdf-pop-resp.py",
+#         "scripts/pdf-pop-rrms.py",
+#         "scripts/pdf-set-charges.py",
+#     ],
+#     install_requires=[
+#         "h5py",
+#         "BeautifulSoup4",
+#         "numpy",
+#         "scipy",
+#         "pandas",
+#         "matplotlib",
+#         "scikit-learn",
+#         # 'proteindf_bridge'
+#     ],
+#     package_data={"proteindf_tools": ["data/basis2.cache"]},
+# )
